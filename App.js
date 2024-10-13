581145eb5c6cd4cd7db8f7c8128a01fc3e64379a
Mohammad Ibrahim <mdiibrahim549@gmail.com>
2024-10-13 22:36:49 +0600

    auth subcriber done

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import auth from "@react-native-firebase/auth";
@@ -8,9 +9,24 @@ import ContactDetailsScreen from "./src/screens/ContactDetailsScreen";
 const Stack = createStackNavigator();
 
 const App = () => {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, []);
+
+  if (initializing) return null;
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName={user ? "Contacts" : "Login"}>
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Contacts" component={ContactsScreen} />
         <Stack.Screen name="ContactDetails" component={ContactDetailsScreen} />
